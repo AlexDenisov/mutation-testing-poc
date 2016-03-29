99076db64d64bb46fbb5fb0eb0f99ed6e914be95
AlexDenisov <1101.debian@gmail.com>
2016-03-29 20:56:29 +0200

    Initial commit

---
+++ Mutang/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Mutang
+//
+//  Created by AlexDenisov on 29/03/16.
+//  Copyright © 2016 lowlevelbits. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
